dbed785b63db596b15615b57c61bfe342934900b
Виктория <viktoria@MacBook-Air-Viktoria.local>
2022-03-28 17:01:19 +0300

    Добавляет главную страницу и форму заявки

--- .eleventy.js
+++ .eleventy.js
@@ -1,11 +1,21 @@
+const yaml = require("js-yaml");
+
 module.exports = function(eleventyConfig) {
-    eleventyConfig.addPassthroughCopy("./src/style.css")
+
+    // To Support .yaml Extension in _data
+  // You may remove this if you can use JSON
+  eleventyConfig.addDataExtension("yaml", (contents) =>
+    yaml.load(contents)
+  );
+
+    eleventyConfig.addPassthroughCopy("./src/css/*")
     eleventyConfig.addPassthroughCopy("./src/img");
+    eleventyConfig.addPassthroughCopy("./src/fonts");
     // Return your Object options:
     return {
       dir: {
         input: "src",
-        output: "public"
+        output: "docs"
       }
     }
   };
--- src/index.md
+++
@@ -1,4 +0,0 @@
----
-layout: "base.njk"
----
-<img class="page-header__logo" src="{{ '/img/logo-studio.svg' | url }}" width="90" height="35" alt="Лепёхин">
