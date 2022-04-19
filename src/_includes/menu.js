3cbeafc9bbc379e971a0b7155515196ae43b416a
Виктория <viktoria@MacBook-Air-Viktoria.local>
2022-04-19 16:20:40 +0300

    мобильная навигация

--- src/_includes/menu.js
+++ src/_includes/menu.js
@@ -1,7 +1,9 @@
+// const page = document.querySelector('.page')
 const header = document.querySelector('.page-header')
 const buttonMenuHendler = header.querySelector('.page-header__open-menu')
 if (buttonMenuHendler) {
     buttonMenuHendler.addEventListener('click',() => {
+        page.classList.toggle('page--open-navigation')
         header.classList.toggle('page-header--open')
     })
 }
