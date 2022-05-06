fdc5875d2775ed4468514dd307afa71bfae8c348
SamoylenkVi <vikapasha14@gmail.com>
2022-05-06 19:15:25 +0300

    Добавляет страницу о нас

--- .eleventy.js
+++ .eleventy.js
@@ -12,6 +12,12 @@ module.exports = function(eleventyConfig) {
     eleventyConfig.addPassthroughCopy("./src/img");
     eleventyConfig.addPassthroughCopy("./src/fonts");
     eleventyConfig.addPassthroughCopy("./src/js/*");
+
+    eleventyConfig.addPassthroughCopy({ "./node_modules/@glidejs/glide/dist/css/glide.core.min.css": "css/vendor/glide.core.min.css" });
+    eleventyConfig.addPassthroughCopy({ "./node_modules/@glidejs/glide/dist/css/glide.theme.min.css": "css/vendor/glide.theme.min.css" });
+    eleventyConfig.addPassthroughCopy({ "./node_modules/@glidejs/glide/dist/glide.modular.esm.js": "js/vendor/glide.js" });
+    eleventyConfig.addPassthroughCopy({ "./node_modules/@glidejs/glide/dist/glide.min.js": "js/vendor/glide.min.js" });
+    
     // Return your Object options:
     return {
       pathPrefix: "/lepekhin.art/",
---
+++ docs/js/slider.js
@@ -0,0 +1,11 @@
+// import Glide from './vendor/glide.js';
+
+// console.log(Glide);
+
+// new Glide('.glide', {
+//     type: 'carousel',
+//     startAt: 0,
+//     perView: 3
+//   })
+
+// new Glide('.glide').mount();
